cdb4bf856efd6792e7dc2696ec2c74fb6f94c2ee
tylerpatzer <84998125+tylerpatzer@users.noreply.github.com>
2023-04-05 23:53:19 -0400

    code commenting and optimization

--- Noble/Assets/Scripts/GameManagement/CloudGenerator.cs
+++ Noble/Assets/Scripts/GameManagement/CloudGenerator.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This class spawns clouds into the scene
 public class CloudGenerator : MonoBehaviour
 {
+    // array of cloud prefabs
     [SerializeField]
     private GameObject[] clouds;
 
+    // The rate at which the clouds will spawn in at
     [SerializeField]
     private float spawnRate;
 
+    // where the clouds will despawn at
     [SerializeField]
     private GameObject endPoint;
 
+    // the start position of the cloud spawner
     private Vector3 startPosition;
 
     // Start is called before the first frame update
@@ -20,13 +25,17 @@ public class CloudGenerator : MonoBehaviour
     {
         startPosition = this.transform.position;
         Prewarm();
-        Invoke("AttemptSpawn", spawnRate); // change to coroutine later
+        StartCoroutine(CloudSpawnScheduler());
     }
 
+    // spawn a new cloud object into the scene
     public void SpawnCloud(Vector3 startPosition)
     {
+        // get the height and scale of the cloud
         float startY = UnityEngine.Random.Range(startPosition.y - 1f, startPosition.y + 1f);
         float scale = UnityEngine.Random.Range(0.8f, 1.2f);
+
+        // instantiate the cloud and set its position and scale
         GameObject cloud = Instantiate(clouds[UnityEngine.Random.Range(0, clouds.Length)]);
         cloud.transform.localScale = new Vector2(scale, scale);
         cloud.transform.position = new Vector3(startPosition.x, startY, startPosition.z);
@@ -35,19 +44,23 @@ public class CloudGenerator : MonoBehaviour
             .StartFloating(UnityEngine.Random.Range(0.5f, 1.5f), endPoint.transform.position.x);
     }
 
-    public void AttemptSpawn()
-    {
-        // checking criteria
-        SpawnCloud(startPosition);
-        Invoke("AttemptSpawn", spawnRate);
-    }
-
+    // preload clouds into the scene so they are there when the game begins
     public void Prewarm()
     {
         for (int i = 0; i < 100; i++)
         {
             Vector3 spawnPos = startPosition + Vector3.right * (i * 2);
             SpawnCloud(spawnPos);
+        }
+    }
+
+    // spawns in clouds at the specified rate
+    public IEnumerator CloudSpawnScheduler()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            SpawnCloud(startPosition);
         }
     }
 }
--- Noble/Assets/Scripts/GameManagement/CloudProperties.cs
+++ Noble/Assets/Scripts/GameManagement/CloudProperties.cs
@@ -2,26 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// this class defines the properties of each cloud object
 public class CloudProperties : MonoBehaviour
 {
+    // how fast the cloud is moving
     private float speed;
+
+    // when the cloud should despawn
     private float endPositionX;
 
-    // Cloud properties Constructor
+    // cloud properties constructor
     public void StartFloating(float s, float x)
     {
         speed = s;
         endPositionX = x;
     }
 
-    // Start is called before the first frame update
-    void Start() { }
-
     // Update is called once per frame
     void Update()
     {
+        // move cloud
         transform.Translate(Vector3.right * (Time.deltaTime * speed));
-        if (transform.position.x > endPositionX)
+
+        // destroy the cloud when applicable
+        if (this.transform.position.x > endPositionX)
         {
             Destroy(this.gameObject);
         }
